a7d99f4cef18df9db563eb1b593baba3ae147989
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-05-03 11:51:57 +0200

    Install linters and fix linter errors.

--- Modules/BookClass.js
+++ Modules/BookClass.js
@@ -1,9 +1,9 @@
 class Book {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-      this.id = Date.now();
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = Date.now();
   }
-  
-  export default Book;
+}
+
+export default Book;
